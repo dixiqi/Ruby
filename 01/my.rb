438542552d1dd1595c0d0616f31e1299f67574af
dingxiaoqian <dingxiaoqian18@mails.ucas.ac.cn>
2019-04-14 16:59:19 +0800

    第一章 简单介绍--

---
+++ 01/my.rb
@@ -0,0 +1,11 @@
+#encoding:gbk
+puts "嗨，大家好啊！"
+today=Time.new
+print "当前的时间是："
+puts today
+print "我是一个Ruby初学者，大家可以叫我："
+p "somebody"
+
+puts "\r\n"
+print "我的个人网站是："
+p "http://www.dingxiaoqianheihei"
